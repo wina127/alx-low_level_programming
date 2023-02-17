c9f9eb416b91f659fffbdfc448dc691b2464eb37
wina127 <wongelalemayhu@gmail.com>
2023-02-16 18:03:12 -0800

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
 *main - Entry point
-*Return: Always 0 (Success)
+*Return: Always 0
 */
-
 int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 {
