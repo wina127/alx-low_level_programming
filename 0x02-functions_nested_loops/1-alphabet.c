ba0882b9cba304479bf8d47b3a4802087374a1c4
wina127 <wongelalemayhu@gmail.com>
2023-02-22 00:07:03 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,13 @@
 /**
  * print_alphabet - print all alphabet in lowercase
  */
-void print_alphabet(void)
 {
+	void print_alphabet(void)
+	{
+		char letter;
 
-	char letter;
-	for (letter= 'a'; letter ,<= 'z'; letter++)
-		_putchar(letter);
-	_putchar('\n');
-}
+		for (letter='a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		
+		_putchar('\n');
+	}
