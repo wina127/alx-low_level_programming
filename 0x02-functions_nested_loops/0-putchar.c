69082c3e644a6c9cf6a6ed0a0d845fea4e6f4e87
wina127 <wongelalemayhu@gmail.com>
2023-02-21 18:44:52 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - verifies the code
+ * Return: 0
+ */
+int main(void/0
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0)'
+}
