b74d793e1cdd1fce7e45257c66a99394eb37db5b
wina127 <wongelalemayhu@gmail.com>
2023-02-22 07:16:53 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,7 +7,7 @@
 	{
 		int i, j;
 
-		i =0;
+		i = 0;
 
 
 		while (i < 24)
@@ -18,7 +18,7 @@
 				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
 				_putchar(':');
-				_putchar((j /10) + '0');
+				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 				_putchar('\n');
 				j++;
