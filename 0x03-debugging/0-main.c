ebb3412b61c593be284c806c3fc41521c0b73ec3
wina127 <wongelalemayhu@gmail.com>
2023-02-23 08:44:14 -0800

    0-main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - test function that prints if integer is positive or negative
+ * Return: Always success
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
