9eaf09c8fa71e69f15c963bc67ce25c55e5244ef
wina127 <wongelalemayhu@gmail.com>
2023-02-23 10:12:53 -0800

    com

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -25,7 +25,7 @@ else
 {
 if (month == 2 && day == 60)
 {
-prinf("invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+prinf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
