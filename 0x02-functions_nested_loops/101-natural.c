3f0fb4c1d7f801db870c4fc3f026be18a23605d3
wina127 <wongelalemayhu@gmail.com>
2023-02-22 16:13:04 -0800

    com

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,11 +3,10 @@
  *  Auth: dave
  *
  */
-
 	#include <stdio.h>
 /**
  *main - Lists all the natural numbers below 1024 (excluded)
- *that are multiples of 3 or 5 
+ *that are multipls of 3or5
  *Return: Always 0
  */
 	int main(void)
