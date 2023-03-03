0953e35cfe7691402de7d0d75db9e2be610c80c3
wina127 <wongelalemayhu@gmail.com>
2023-03-02 17:05:05 -0800

    0x06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
+
 /**
- * cap_string - this capitalizes all words of a string.
- * @str: this is the string to be capitalized.
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
  *
  * Return: A pointer to the changed string.
  */
@@ -10,29 +11,25 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
+	/* Capitalize first letter */
+	if (str[index] >= 'a' && str[index] <= 'z')
+		str[index] -= 32;
+
 	while (str[index])
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
-			str[index] -= 32;
+		/* Check for word separators */
+		if (str[index] == ' ' || str[index] == '\t' ||
+		    str[index] == '\n' || str[index] == ',' ||
+		    str[index] == ';' || str[index] == '.' ||
+		    str[index] == '!' || str[index] == '?' ||
+		    str[index] == '"' || str[index] == '(' ||
+		    str[index] == ')' || str[index] == '{' ||
+		    str[index] == '}')
+		{
+			/* Capitalize next letter if it's lowercase */
+			if (str[index + 1] >= 'a' && str[index + 1] <= 'z')
+				str[index + 1] -= 32;
+		}
 
 		index++;
 	}
-
-	return (str);
-}
