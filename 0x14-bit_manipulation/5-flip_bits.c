95d4c49877c6b2fbbf31c038eee065537fc78e4b
wina127 <wongelawitalemayhu@gmail.com>
2023-05-04 19:10:08 -0700

    a function that returns the number of bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - sets the value of a bit to 0 at a given index
+ * @n: a pointer to the number to modify
+ * @m: the index of the bit to clear
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int flip = n ^ m;
+	unsigned int counr = 0;
+
+	while (flip != 0)
+	{
+		if (flip & 1)
+			count++;
+		flip >>= 1;
+	}
+	return (count);
+}
