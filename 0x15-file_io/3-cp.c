5e5c231a7a13db22ef6928af35e4d750dbb1cc52
wina127 <wongelawitalemayhu@gmail.com>
2023-05-27 07:57:48 -0700

    3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,10 +22,10 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "usages: cp file_from file_to\n"), exit(97);
 	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (to == -1)
-		dprintf(STDERR_FILENO, WRITE_ERR, arg[2]), exit(99);
+		dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
-		dprintf(STDDERR_FILENO, READ_ERR, arg[1]), exit(98);
+		dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
 	while (1)
 	{
 		r = read(from, buffer, 1024);
