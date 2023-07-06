439e3acef42fd60d253970a79ab51e0d439ae4e1
wina127 <wongelawitalemyhu@gmail.com>
2023-07-06 16:45:50 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * get_endianness - function
+ * Return: return int
+ */
+int get_endianness(void)
+{
+	unsigned int x = 1;
+	char *c = (char *) &x;
+
+	return (*c == 1);
+}
