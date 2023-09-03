3b901bcf99cd173738aa139ef02b1f532a31513c
wina127 <122681290+wina127@users.noreply.github.com>
2023-09-03 12:56:15 -0600

    Delete 0x18-dynamic_libraries directory

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -p .. https://raw.githubsercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x018-dynamic_libraries/libgiga.so 
-export LD_PRELOAD="$PWD/../libgiga.so"
