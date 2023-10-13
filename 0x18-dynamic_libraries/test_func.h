fc9c4260bd82e58732b7277b837e26d2ac79e58b
wina127 <wongelawitalemayhu@gmail.com>
2023-10-12 17:10:12 -0700

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/test_func.h
@@ -0,0 +1,12 @@
+#ifndef TEST_FUNC_H
+#define TEST_FUNC_H
+
+/* Function prototypes */
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif /* TEST_FUNC_H */
+
